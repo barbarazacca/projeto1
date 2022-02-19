16ff42eaa87c06ebe5bf6754e1f2671eaf418490
Bárbara <barbara@MacBook-Pro-de-caio-2.local>
2022-02-19 14:37:12 -0300

    Second ViewController

---
+++ projeto1/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  projeto1
+//
+//  Created by Bárbara on 19/02/22.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
